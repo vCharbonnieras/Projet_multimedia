48e51c78c272caba911083da4a04b642375b06c9
Vincent Nguyen <ving10785@ma-3020-lx>
2019-05-16 11:23:10 +0200

    Réécriture pour adapter à main_console. Ajout usage de la commande stitching.

---
+++ cv_stitching/cv_stitching.h
@@ -0,0 +1,8 @@
+#ifndef __CV_STITCHING_CPP__
+#define __CV_STITCHING_CPP__
+
+int loadImage(String name, bool sep);
+int parseCmdArgs(int argc, char** argv);
+int stitching(int argc, char** argv); // to export only
+
+#endif
